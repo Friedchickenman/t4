8b70a66a2e046f5f746dc152ecda67185a0b2ae6
SungJun <psj12343@gmail.com>
2025-10-16 00:12:04 +0900

    10/15 React Router 23th commit

--- src/components/todo/readComponent.tsx
+++ src/components/todo/readComponent.tsx
@@ -14,15 +14,22 @@ function ReadComponent() {
 
     const [todo, setTodo] = useState<Todo>(initState)
 
-    const {tno} = useParams()
+    const params = useParams(); //useParams에서 가져옴
+
+    const tnoStr = params.tno
 
     useEffect(() => {
 
+        const tno = Number(tnoStr);
+
+        console.log("tno:" , tno)
+
         getTodo(tno).then(data => {
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
             setTodo(data)
         })
 
-    }, [tno]);
+    }, [tnoStr]);
 
     return (
         <div>Todo Read Component</div>
