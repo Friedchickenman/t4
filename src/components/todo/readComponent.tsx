279c2b5c89226f3898ccb58161e0d3067302846f
SungJun <psj12343@gmail.com>
2025-10-15 23:49:12 +0900

    10/15 React Router 22th commit

--- src/components/todo/readComponent.tsx
+++ src/components/todo/readComponent.tsx
@@ -1,5 +1,29 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getTodo} from "../../api/todoApi.tsx";
+
+const initState:Todo = {
+    tno:0,
+    title:'',
+    writer:'',
+    regDate:'',
+    modDate:''
+}
 
 function ReadComponent() {
+
+    const [todo, setTodo] = useState<Todo>(initState)
+
+    const {tno} = useParams()
+
+    useEffect(() => {
+
+        getTodo(tno).then(data => {
+            setTodo(data)
+        })
+
+    }, [tno]);
+
     return (
         <div>Todo Read Component</div>
     );
